d2c572b9d8503fd83023cbc23bbd6629efba9b21
Billy Zoellers <billy.zoellers@mac.com>
2018-09-28 02:52:08 +0000

    added troubleshooting functionality

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -18,6 +18,10 @@ class UnitsController < ApplicationController
 
   end
   
+  def troubleshoot
+          @unit = Unit.find(params[:unit_id])
+  end
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_unit
